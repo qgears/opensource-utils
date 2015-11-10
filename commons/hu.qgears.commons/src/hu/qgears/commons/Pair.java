6182a4727fec7bb22e0123b5c9084d857722e8ea
rizsi <andras.schmidt.hu@gmail.com>
2015-11-10 16:05:17 +0100

    Implement pair as hash key

--- commons/hu.qgears.commons/src/hu/qgears/commons/Pair.java
+++ commons/hu.qgears.commons/src/hu/qgears/commons/Pair.java
@@ -2,7 +2,7 @@ package hu.qgears.commons;
 
 /**
  * DTO for a pair of obejcts.
- * Does not override equals!
+ * equals and hashCode is overridden in a way that it can be used in hashmaps as complex key.
  * @author rizsi
  *
  * @param <T>
@@ -12,6 +12,8 @@ public class Pair <T,U>
 {
 	private T a;
 	private U b;
+	private boolean hashInited;
+	private int hashCode;
 	public Pair(T a, U b) {
 		super();
 		this.a = a;
@@ -22,5 +24,31 @@ public class Pair <T,U>
 	}
 	public U getB() {
 		return b;
+	}
+	@Override
+	public int hashCode() {
+		if(!hashInited)
+		{
+			hashInited=true;
+			hashCode=h(a)^h(b);
+		}
+		return hashCode;
+	}
+	private int h(Object o) {
+		if(o==null)
+		{
+			return 0;
+		}
+		return o.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Pair<?, ?>)
+		{
+			Pair<?, ?> other=(Pair<?, ?>) obj;
+			return UtilEquals.safeEquals(other.getA(), a)&&
+					UtilEquals.safeEquals(other.getB(), b);
+		}
+		return super.equals(obj);
 	}
 }
--- commons/hu.qgears.commons/src/hu/qgears/commons/UtilEquals.java
+++ commons/hu.qgears.commons/src/hu/qgears/commons/UtilEquals.java
@@ -39,4 +39,42 @@ public class UtilEquals {
 		}
 		return false;
 	}
+	/**
+	 * Safe compare for equals:
+	 * null equals null
+	 * null does not equal anything else
+	 * if objA is not null its equal is called to objB
+	 * @param objA
+	 * @param objB
+	 * @return
+	 */
+	public static boolean safeEquals(Object objA, Object objB) {
+		try {
+			if(objA.getClass().equals(objB.getClass()))
+			{
+				Field[] fd=objA.getClass().getFields();
+				for(Field f: fd)
+				{
+					Object a=f.get(objA);
+					Object b=f.get(objB);
+					if(a==null)
+					{
+						if(b!=null)
+						{
+							return false;
+						}
+					}else
+					{
+						if(!a.equals(b))
+						{
+							return false;
+						}
+					}
+				}
+				return true;
+			}
+		} catch (Exception e) {
+		}
+		return false;
+	}
 }
