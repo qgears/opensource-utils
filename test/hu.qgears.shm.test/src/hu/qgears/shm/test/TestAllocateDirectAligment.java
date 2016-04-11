bd1778604507fdda4a6ff177e813e37227a5799f
Krisztián Tresó <chreex@qgears.com>
2016-04-11 15:54:45 +0200

    unit test documentation

--- test/hu.qgears.shm.test/src/hu/qgears/shm/test/TestAllocateDirectAligment.java
+++ test/hu.qgears.shm.test/src/hu/qgears/shm/test/TestAllocateDirectAligment.java
@@ -14,22 +14,29 @@ import org.junit.Test;
 
 /**
  * Test that Java allocated direct memory must be 16 byte aligned.
+ * 
  * @author rizsi
  *
  */
 public class TestAllocateDirectAligment {
+
+	/**
+	 * Verifies that direct memory, allocated by the JVM, is aligned to 16 
+	 * bytes.
+	 */
 	@Test
-	public void testAllocationAlignment()
-	{
+	public void testAllocationAlignment() {
 		UtilSharedMemory.getInstance();
-		Random r=new Random();
-		List<INativeMemory> mems=new ArrayList<INativeMemory>();
-		for(int i=0;i<10000;++i)
-		{
-			INativeMemory mem=DefaultJavaNativeMemoryAllocator.getInstance().allocateNativeMemory(r.nextInt(1009)+1);
+		Random r = new Random();
+		List<INativeMemory> mems = new ArrayList<INativeMemory>();
+		for (int i = 0; i < 10000; ++i) {
+			INativeMemory mem = DefaultJavaNativeMemoryAllocator.getInstance()
+					.allocateNativeMemory(r.nextInt(1009) + 1);
 			mems.add(mem);
-			PartNativeMemory pnm=new PartNativeMemory(mem, 0, 1);
-			Assert.assertTrue("Java allocated direct memory must be 16 byte aligned.", pnm.getNativePointer1()%16==0);
+			PartNativeMemory pnm = new PartNativeMemory(mem, 0, 1);
+			Assert.assertTrue(
+					"Java allocated direct memory must be 16 byte aligned.",
+					pnm.getNativePointer1() % 16 == 0);
 		}
 	}
 }
