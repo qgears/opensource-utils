66b0d9e88f7e141499af4740496e2aa03581b7b0
Krisztián Tresó <chreex@qgears.com>
2016-05-30 15:23:10 +0200

    adding method for filling rectangle with arbitrary color

--- commons/hu.qgears.images/src/hu/qgears/images/NativeImageEditor.java
+++ commons/hu.qgears.images/src/hu/qgears/images/NativeImageEditor.java
@@ -23,67 +23,108 @@ public class NativeImageEditor {
 	}
 	
 	/**
-	 * Replaces the pixels within specified rectangular area with black pixels
-	 * (rgb(0,0,0)).
-	 * 
-	 * @param topLeftX
-	 * @param topLeftY
-	 * @param width
-	 * @param height
+	 * Fills a rectangle with given color data. If the
+	 * {@link NativeImage#getComponentOrder() component order / type} of the
+	 * edited image is {@link ENativeImageComponentOrder#MONO MONO}, only the 
+	 * {@link RGBAColor#a alpha component} will be used as the color; the rest 
+	 * of the color components will be discarded. The only formats supported by 
+	 * this method are: {@link ENativeImageComponentOrder#RGB RGB},
+	 * {@link ENativeImageComponentOrder#RGBA RGBA} and 
+	 * {@link ENativeImageComponentOrder#MONO MONO} - note that no type checking
+	 * will be performed!
+	 * @param topLeftX the left coordinate of the rectangle to fill
+	 * @param topLeftY the top coordinate of the rectangle to fill
+	 * @param width the width of the rectangle to fill
+	 * @param height the height of the rectangle to fill
+	 * @return this editor for method chaining
+	 * @since 6.2
 	 */
-	public NativeImageEditor applyDontcareRegion(int topLeftX, int topLeftY, int width, int height){
-		byte zByte = toByte(0);
-		doFill(zByte , zByte, zByte, zByte, topLeftX, topLeftY, width, height);
+	public NativeImageEditor fillRect(final int topLeftX, final int topLeftY,
+			final int width, final int height, final RGBAColor color) {
+		switch (imageToEdit.getComponentOrder()) {
+		case MONO: {
+			doFillMono(topLeftX, topLeftY, width, height, toByte(color.a));
+			break;
+		}
+		case RGB:
+		case RGBA: {
+			doFillGeneric(toByte(color.r), toByte(color.g), toByte(color.b), 
+					toByte(color.a), topLeftX, topLeftY, width, height);
+		}
+		default:
+			throw new RuntimeException("color fill is not supported for this "
+					+ "pixel order :" + imageToEdit.getComponentOrder());
+		}
+
 		return this;
 	}
 	
+	/**
+	 * Replaces the pixels within specified rectangular area with black pixels
+	 * (rgb(0,0,0)). If an alpha channel is present, the relevant pixels will be
+	 * filled with zeros, too. {@link ENativeImageComponentOrder#MONO MONO} 
+	 * images are not supported by this method!
+	 * 
+	 * @param topLeftX the leftmost coordinate of the rectangle to be filled
+	 * @param topLeftY the topmost coordinate of the rectangle to be filled
+	 * @param width the width of the rectangle to be filled
+	 * @param height the height of the rectangle to be filled
+	 * @return this editor for method chaining
+	 */
+	public NativeImageEditor applyDontcareRegion(int topLeftX, int topLeftY, 
+			int width, int height) {
+		byte zByte = toByte(0);
+		
+		doFillGeneric(zByte, zByte, zByte, zByte, topLeftX, topLeftY, width, height);
+		
+		return this;
+	}
 	
 	/**
 	 * Fills all pixels of image with specified color.
 	 * 
-	 * @param backgroundColor
-	 * @return
+	 * @param color
+	 * @return this editor for method chaining
 	 * @since 3.0
 	 */
-	public NativeImageEditor fillWithColor(RGBAColor backgroundColor){
-		ENativeImageComponentOrder co = imageToEdit.getComponentOrder();
+	public NativeImageEditor fillWithColor(final RGBAColor color) {
+		final ENativeImageComponentOrder co = imageToEdit.getComponentOrder();
+		
 		switch (co) {
 		case BGR :
 		case BGRA: {
-			doFill(toByte(backgroundColor.b), toByte(backgroundColor.g),
-					toByte(backgroundColor.r), toByte(backgroundColor.a));
+			doFill(toByte(color.b), toByte(color.g),
+					toByte(color.r), toByte(color.a));
 			break;
 		}
 		case RGB :
 		case RGBA: {
-			doFill(toByte(backgroundColor.r), toByte(backgroundColor.g),
-					toByte(backgroundColor.b), toByte(backgroundColor.a));
+			doFill(toByte(color.r), toByte(color.g),
+					toByte(color.b), toByte(color.a));
 			break;
 		}
 		case ABGR: {
-			doFill(toByte(backgroundColor.a), toByte(backgroundColor.b),
-					toByte(backgroundColor.g), toByte(backgroundColor.r));
+			doFill(toByte(color.a), toByte(color.b),
+					toByte(color.g), toByte(color.r));
 			break;
 		}
 		case ARGB: {
-			doFill(toByte(backgroundColor.a), toByte(backgroundColor.r),
-					toByte(backgroundColor.g), toByte(backgroundColor.b));
+			doFill(toByte(color.a), toByte(color.r),
+					toByte(color.g), toByte(color.b));
 			break;
 		}
 		case ALPHA: {
-			imageToEdit.clearAlpha((byte) backgroundColor.a);
+			imageToEdit.clearAlpha((byte) color.a);
 			break;
 		}
 		case MONO:{
-			for (int i = 0; i < imageToEdit.getWidth(); i++) {
-				for (int j = 0; j < imageToEdit.getHeight(); j++) {
-					imageToEdit.setChannel(i, j, 0, toByte (backgroundColor.a));
-				}
-			}
+			doFillMono(0, 0, imageToEdit.getWidth(), imageToEdit.getHeight(), 
+					toByte(color.a));
 			break;
 		}
 		default:
-			throw new RuntimeException("background color fill is not supported for this pixel order :"+co);
+			throw new RuntimeException("background color fill is not supported "
+					+ "for this pixel order :"+co);
 		}
 		return this;
 	}
@@ -92,15 +133,32 @@ public class NativeImageEditor {
 		return (byte) (i & 0xFF);
 	}
 	
-	private void doFill(byte first, byte second, byte third, byte fourth){
-		doFill(first, second, third, fourth,0,0,imageToEdit.getWidth(), imageToEdit.getHeight());
+	private void doFill(byte first, byte second, byte third, byte fourth) {
+		doFillGeneric(first, second, third, fourth, 
+				0, 0, imageToEdit.getWidth(), imageToEdit.getHeight());
 	}
-	/*
-	 * too many parameters is OK here
+	
+	/**
+	 * Fills a rectangle with given color data. Note that the names of 
+	 * parameters called '{@code first}', '{@code second}', '{@code third}', 
+	 * and '{@code fourth}' refer to the in-pixel offset of a color byte. Note 
+	 * that {@link ENativeImageComponentOrder#MONO MONO} images are not 
+	 * supported by this method.
+	 *  
+	 * @param first the color byte at pixel offset + 0
+	 * @param second the color byte at pixel offset + 1
+	 * @param third the color byte at pixel offset + 2
+	 * @param fourth the color byte at pixel offset + 3
+	 * @param x the left coordinate of the rectangle to fill
+	 * @param y the top coordinate of the rectangle to fill
+	 * @param width the width of the rectangle to fill
+	 * @param height the height of the rectangle to fill
 	 */
-	private void doFill(byte first, byte second, byte third, byte fourth, int x, int y, int width, int height){//NOSONAR
-		for (int i = x; i< x+width; i++){
-			for(int j = y; j< y+height; j++){
+	/* too many parameters are OK here */
+	private void doFillGeneric(byte first, byte second, byte third, byte fourth,
+			int x, int y, int width, int height) { // NOSONAR
+		for (int i = x; i < x + width; i++) {
+			for (int j = y; j < y + height; j++) {
 				imageToEdit.setChannel(i, j, 0, first);
 				imageToEdit.setChannel(i, j, 1, second);
 				imageToEdit.setChannel(i, j, 2, third);
@@ -110,6 +168,26 @@ public class NativeImageEditor {
 			}
 		}
 	}
+	
+	/**
+	 * Fills a rectangle with given color data. Note that only 
+	 * {@link ENativeImageComponentOrder#MONO MONO} images are  supported by 
+	 * this method.
+	 *  
+	 * @param x the left coordinate of the rectangle to fill
+	 * @param y the top coordinate of the rectangle to fill
+	 * @param width the width of the rectangle to fill
+	 * @param height the height of the rectangle to fill
+	 * @param color the color value, ranging from  
+	 */
+	private void doFillMono(int x, int y, int width, int height, byte color) {
+		for (int i = x; i < x + width; i++) {
+			for (int j = y; j < y + height; j++) {
+				imageToEdit.setChannel(i, j, 0, color);
+			}
+		}
+	}
+	
 	/**
 	 * Returns the underlying image, that is edited using this editor.
 	 * 
