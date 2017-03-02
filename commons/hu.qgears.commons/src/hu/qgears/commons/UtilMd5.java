1e1e0e286710990afce578b0a9f44eac86904e32
rizsi <andras.schmidt.hu@gmail.com>
2017-03-02 14:30:25 +0100

    Md5 hash utils.

---
+++ commons/hu.qgears.commons/src/hu/qgears/commons/UtilMd5.java
@@ -0,0 +1,71 @@
+package hu.qgears.commons;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Md5 hash helper utilities.
+ */
+public class UtilMd5 {
+	/**
+	 * Length of an MD5 hash Hex format in characters.
+	 */
+	public static final int HASHLENGTH=32;
+	/**
+	 * Md5 hash the string (transformed to binary using UTF-8)
+	 * @param f
+	 * @return
+	 */
+	public static String getMd5(String f)
+	{
+		try {
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			m.update(f.getBytes(StandardCharsets.UTF_8));
+			return UtilString.padLeft(new BigInteger(1, m.digest()).toString(16), HASHLENGTH, '0');
+		} catch (NoSuchAlgorithmException e) {
+			// Never happens as MD5 is part of JVM
+			throw new RuntimeException(e);
+		}
+
+	}
+	/**
+	 * MD5 hash of the 'file'.
+	 * @param f
+	 * @return
+	 */
+	public static String getMd5(byte[] f)
+	{
+		try {
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			m.update(f);
+			return UtilString.padLeft(new BigInteger(1, m.digest()).toString(16), HASHLENGTH, '0');
+		} catch (NoSuchAlgorithmException e) {
+			// Never happens as MD5 is part of JVM
+			throw new RuntimeException(e);
+		}
+	}
+	/**
+	 * Create an md5 hash digest. Exceptions are transformed to {@link RuntimeException} inside.
+	 * @return
+	 */
+	public static MessageDigest createMd5()
+	{
+		try {
+			return MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			// Never happens as MD5 is part of JVM
+			throw new RuntimeException(e);
+		}
+	}
+	/**
+	 * Finish the digest and convert to String.
+	 * @param m
+	 * @return
+	 */
+	public static String toMd5String(MessageDigest m)
+	{
+		return UtilString.padLeft(new BigInteger(1, m.digest()).toString(16), HASHLENGTH, '0');
+	}
+}
