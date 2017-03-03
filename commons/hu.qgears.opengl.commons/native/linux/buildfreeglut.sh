55a06121ab372f9603e4273f7771fe54a115ff9c
Krisztián Tresó <chreex@qgears.com>
2017-03-03 17:34:40 +0100

    compatibility upgrade for Ubuntu 14.04 and above

--- commons/hu.qgears.opengl.commons/native/linux/buildfreeglut.sh
+++ commons/hu.qgears.opengl.commons/native/linux/buildfreeglut.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BUILDDIR=$1
 TARGETDIR=$2
@@ -9,6 +9,15 @@ cp -r ../freeglut-2.6.0/* $BUILDDIR
 
 cd $BUILDDIR
 ./configure
+
+# Upgrades old build scripts on Ubuntu 14.04 and above
+LSB_RELEASE=$(lsb_release -rs)
+if [ $(echo "$LSB_RELEASE >= 14.04" | bc -l) ]; then
+	echo "Upgrading build scripts for Ubuntu 14 and above..." 
+	autoreconf -vif
+	echo "done"
+fi
+
 make clean
 make
 
