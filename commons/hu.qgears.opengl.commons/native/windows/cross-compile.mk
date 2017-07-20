c1c4d30464832cf2c396490650d861cbe56be746
jogaproger <jogaproger@gmail.com>
2017-07-20 14:19:22 +0200

    opengl cross compile makefile for 32 and 64 bit

--- commons/hu.qgears.opengl.commons/native/windows/cross-compile.mk
+++ commons/hu.qgears.opengl.commons/native/windows/cross-compile.mk
@@ -1,38 +1,44 @@
 #
 #
-#	libqglut64.dll makefile for msys2
+#	libqglut(32|64).dll makefile for msys2
 #
 #
 
-WIN64DEP = ../../win64-dep
-JDKPATH=$(WIN64DEP)/JDK-win
-MINGW64=$(WIN64DEP)/mingw64
-
+WINDEP = ../../win-dep
+JDKPATH=$(WINDEP)/JDK-win
+MINGW32=$(WINDEP)/mingw32
+MINGW64=$(WINDEP)/mingw64
 
 JNI_INCLUDE=\
 -I$(JDKPATH)/include \
 -I$(JDKPATH)/include/win32 \
 
-ARCHPOSTFIX='64'
-TOOLCHAIN = x86_64-w64-mingw32
-
 #PKGCONFIG=`pkg-config freeglut glew --libs`
-PKGCONFIG=-L$(MINGW64)/lib -lfreeglut -lopengl32 -lwinmm -lgdi32 -lm -lglew32
-
-DLLS = GLEW32.DLL LIBFREEGLUT.DLL
+PKG_DIR_32=-I$(MINGW32)/include -L$(MINGW32)/lib
+PKG_DIR_64=-I$(MINGW64)/include -L$(MINGW64)/lib
+PKG_LIB=-lfreeglut -lopengl32 -lwinmm -lgdi32 -lm -lglew32
 
 #used by the maven editor_platform_build process, see pom.xml in project.
 ifndef OUTPUTDIR
     OUTPUTDIR = ../../src/hu/qgears/opengl/glut
 endif
 
-gnu_c:
-	$(TOOLCHAIN)-gcc -m64 -o $(OUTPUTDIR)/libqglut$(ARCHPOSTFIX).dll \
-	-fPIC -D_REENTRANT -shared -Wl,--add-stdcall-alias \
+COMMAND_32 =   i686-w64-mingw32-gcc -m32 -o $(OUTPUTDIR)/libqglut32.dll $(PKG_DIR_32) 
+COMMAND_64 = x86_64-w64-mingw32-gcc -m64 -o $(OUTPUTDIR)/libqglut64.dll $(PKG_DIR_64) 
+
+COMMAND_COMMON = -fPIC -D_REENTRANT -shared -Wl,--add-stdcall-alias \
 	${JNI_INCLUDE} \
-	-I$(MINGW64)/include \
 	-DQGLUT_MSYS \
 	../QGlut_msys.cpp ../jniutil.cpp \
-	$(PKGCONFIG) -lglu32 \
+	$(PKG_LIB) -lglu32 \
 	-static-libgcc -Wl,-Bstatic -lgcc -lstdc++ -lpthread 
 
+.PHONY: all
+
+all: compile32 compile64
+	
+compile32:
+	$(COMMAND_32) $(COMMAND_COMMON)
+	
+compile64:
+	$(COMMAND_64) $(COMMAND_COMMON)
