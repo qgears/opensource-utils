2ebdc8342de4769f39aaf84c3a510a055d7beb10
Krisztián Tresó <chreex@qgears.com>
2018-04-06 17:18:32 +0200

    printing os.version

--- commons/hu.qgears.nativeloader/src/hu/qgears/nativeloader/DumpOsNameAndArch.java
+++ commons/hu.qgears.nativeloader/src/hu/qgears/nativeloader/DumpOsNameAndArch.java
@@ -15,8 +15,10 @@ public class DumpOsNameAndArch {
 	private void run() {
 		String archKey = "os.arch";
 		String oskey = "os.name";
+		String osVersionKey = "os.version";
 		String arch = System.getProperty(archKey);
 		String os = System.getProperty(oskey);
+		String osVersion = System.getProperty(osVersionKey);
 		String key = os + "." + arch;
 		/*
 		 * This is a console application
@@ -24,5 +26,6 @@ public class DumpOsNameAndArch {
 		System.out.println("OS   :'"+os+"'");//NOSONAR
 		System.out.println("ARCH :"+arch);//NOSONAR
 		System.out.println("KEY  :"+key);//NOSONAR
+		System.out.println("VER  :"+osVersion);//NOSONAR
 	}
 }
