64fbc9704e4082faeee40b3542882255bd0f1e54
Adam Horvath <adam@qgears.com>
2018-06-20 16:48:09 +0200

    Add function for escaping ANSI C strings

--- commons/hu.qgears.commons/src/hu/qgears/commons/UtilEscape.java
+++ commons/hu.qgears.commons/src/hu/qgears/commons/UtilEscape.java
@@ -51,4 +51,64 @@ public class UtilEscape {
 		}
 		return ret.toString();
 	}
+	
+	/**
+	 * Escapes string to be writable in an ANSI C char* variable.
+	 * 
+	 * @param str
+	 * @return the string as to be written in a char* variable in C
+	 */
+	public static String escapeToANSICString(String str)
+	{
+		StringBuilder ret=new StringBuilder();
+		for(int i=0;i<str.length();++i)
+		{
+			char ch=str.charAt(i);
+			if(ch=='\n')
+			{
+				ret.append("\\n");
+			}
+			else if(ch=='\'')
+			{
+				ret.append("\\\'");
+			}
+			else if(ch=='\"')
+			{
+				ret.append("\\\"");
+			}
+			else if(ch=='\\')
+			{
+				ret.append("\\\\");
+			}
+			else if(ch=='\r')
+			{
+				ret.append("\\r");
+			}else if (ch=='\t')
+			{
+				ret.append("\\t");
+			}
+			else if (ch=='?')
+			{
+				ret.append("\\?");
+			}
+			else if (ch=='\f')
+			{
+				ret.append("\\f");
+			}
+			else if (ch=='\b')
+			{
+				ret.append("\\b");
+			}
+			else if(ch<' '||ch > 0x7f)
+			{
+				ret.append("\\u");
+				ret.append(UtilString.padLeft(Integer.toHexString(ch), 4, '0'));
+			}
+			else
+			{
+				ret.append(ch);
+			}
+		}
+		return ret.toString();
+	}
 }
--- test/hu.qgears.commons.test/src/hu/qgears/commons/test/TestUtilEscape.java
+++ test/hu.qgears.commons.test/src/hu/qgears/commons/test/TestUtilEscape.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 public class TestUtilEscape {
 	static String src="\\\n\r\'\"";
+	static String src_c="\\\n\r\'\"\t?";
 	@Test
 	public void test()
 	{
@@ -15,6 +16,16 @@ public class TestUtilEscape {
 		Assert.assertEquals(src, s);
 		String out=UtilEscape.escapeToJavaString(src);
 		String ref="\\\\\\n\\r\\\'\\\"";
+		Assert.assertEquals(ref, out); 
+	}
+	
+	@Test
+	public void testANSIC()
+	{
+		String s="\\\n\r\'\"\t?";
+		Assert.assertEquals(src_c, s);
+		String out=UtilEscape.escapeToANSICString(src_c);
+		String ref="\\\\\\n\\r\\\'\\\"\\t\\?";
 		Assert.assertEquals(ref, out); 
 	}
 }
