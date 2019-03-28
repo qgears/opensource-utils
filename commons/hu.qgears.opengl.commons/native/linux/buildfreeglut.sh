952253f7592b7539840b9e846e0f2b5a4e0b0f8c
Krisztián Tresó <chreex@qgears.com>
2019-03-28 12:40:20 +0100

    fixing command ordering

--- commons/hu.qgears.opengl.commons/native/linux/buildfreeglut.sh
+++ commons/hu.qgears.opengl.commons/native/linux/buildfreeglut.sh
@@ -7,6 +7,8 @@ rm -fr $BUILDDIR
 mkdir -p $BUILDDIR
 cp -r ../freeglut-2.6.0/* $BUILDDIR
 
+cd $BUILDDIR
+
 # Upgrades old build scripts on Ubuntu 14.04 and above
 LSB_RELEASE=$(lsb_release -rs)
 if [ $(echo "$LSB_RELEASE >= 14.04" | bc -l) ]; then
@@ -15,7 +17,6 @@ if [ $(echo "$LSB_RELEASE >= 14.04" | bc -l) ]; then
 	echo "done"
 fi
 
-cd $BUILDDIR
 ./configure
 
 make clean
