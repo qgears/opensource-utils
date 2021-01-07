e1b71bf6ccd9a4ce8ffd189919b3c0b121110918
Krisztián Tresó <krisztian.treso@qgears.com>
2021-01-07 11:46:41 +0100

    removing watching stdin, writing errors to stderr
    & removing debug-purpose struct duplications

--- commons/hu.qgears.opengl.commons/kms_gl/drm-legacy.c
+++ commons/hu.qgears.opengl.commons/kms_gl/drm-legacy.c
@@ -29,6 +29,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <sys/socket.h>
 
 #include <gbm.h>
 #include <sys/stat.h>
@@ -48,55 +49,6 @@ static drmEventContext evctx = {
 	};
 static struct gbm_bo *bo;
 static struct drm_fb *fb;
-	
-
-struct gbm_device {
-   /* Hack to make a gbm_device detectable by its first element. */
-   struct gbm_device *(*dummy)(int);
-
-   int fd;
-   const char *name;
-   unsigned int refcount;
-   struct stat stat;
-
-   void (*destroy)(struct gbm_device *gbm);
-   int (*is_format_supported)(struct gbm_device *gbm,
-                              uint32_t format,
-                              uint32_t usage);
-
-   struct gbm_bo *(*bo_create)(struct gbm_device *gbm,
-                               uint32_t width, uint32_t height,
-                               uint32_t format,
-                               uint32_t usage);
-   struct gbm_bo *(*bo_import)(struct gbm_device *gbm, uint32_t type,
-                               void *buffer, uint32_t usage);
-   int (*bo_write)(struct gbm_bo *bo, const void *buf, size_t data);
-   void (*bo_destroy)(struct gbm_bo *bo);
-
-   struct gbm_surface *(*surface_create)(struct gbm_device *gbm,
-                                         uint32_t width, uint32_t height,
-                                         uint32_t format, uint32_t flags);
-   struct gbm_bo *(*surface_lock_front_buffer)(struct gbm_surface *surface);
-   void (*surface_release_buffer)(struct gbm_surface *surface,
-                                  struct gbm_bo *bo);
-   int (*surface_has_free_buffers)(struct gbm_surface *surface);
-   void (*surface_destroy)(struct gbm_surface *surface);
-};
-
-struct gbm_surface {
-   struct gbm_device *gbm;
-   uint32_t width;
-   uint32_t height;
-   uint32_t format;
-   uint32_t flags;
-};
-
-struct gbm_kms_surface {
-    struct gbm_surface base;
-    struct gbm_kms_bo *bo[2];
-    int front;
-    int (*set_bo)(struct gbm_kms_surface *, int, void *, int, uint32_t);
-};
 
 static void page_flip_handler(int fd, unsigned int frame,
 		  unsigned int sec, unsigned int usec, void *data)
@@ -167,7 +119,7 @@ int legacy_firstframe(const struct gbm *gbm, const struct egl *egl)
 	int ret = drmModeSetCrtc(drm.fd, drm.crtc_id, fb->fb_id, 0, 0,
 			&drm.connector_id, 1, drm.mode);
 	if (ret) {
-		printf("failed to set mode: %s\n", strerror(errno));
+		fprintf(stderr, "failed to set mode: %s\n", strerror(errno));
 		return ret;
 	}
 	return 0;
@@ -175,80 +127,74 @@ int legacy_firstframe(const struct gbm *gbm, const struct egl *egl)
 
 int legacy_nextframe(const struct gbm *gbm, const struct egl *egl)
 {
-    	fd_set fds;
+fd_set fds;
 
-		struct gbm_bo *next_bo;
-		int waiting_for_flip = 1;
+	struct gbm_bo *next_bo;
+	int waiting_for_flip = 1;
 
-//		egl->draw(i++);
+	eglSwapBuffers(egl->display, egl->surface);
+	
+	next_bo = gbm_surface_lock_front_buffer(gbm->surface);
+	
+    if (!next_bo) {
+       fprintf(stderr, "legacy_nextframe: Failed to get next framebuffer BO; thread_id: %d\n", syscall(__NR_gettid));
+       fflush(stderr);
+       exit(-1);
+    }
 
-		eglSwapBuffers(egl->display, egl->surface);
-		
-		fprintf(stderr, "legacy_nextframe on thread %d\n", syscall(__NR_gettid));
-        print_trace();
-		
-		next_bo = gbm_surface_lock_front_buffer(gbm->surface);
-		
-        if (!next_bo) {
-           fprintf(stderr, "XXX legacy_nextframe: Failed to get next framebuffer BO; thread_id: %d\n", syscall(__NR_gettid));
-           exit(-1);
-        }
-    
-		fb = drm_fb_get_from_bo(next_bo);
-		if (!fb) {
-			fprintf(stderr, "legacy_nextframe: Failed to get a new framebuffer\n");
-			exit(-1);
-		}
+	fb = drm_fb_get_from_bo(next_bo);
+	if (!fb) {
+		fprintf(stderr, "legacy_nextframe: Failed to get a new framebuffer\n");
+        fflush(stderr);
+		exit(-1);
+	}
 
-		/*
-		 * Here you could also update drm plane layers if you want
-		 * hw composition
-		 */
+	/*
+	 * Here you could also update drm plane layers if you want
+	 * hw composition
+	 */
 
-		int ret = drmModePageFlip(drm.fd, drm.crtc_id, fb->fb_id,
-				DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
-		if (ret) {
-			printf("failed to queue page flip: %s\n", strerror(errno));
+	int ret = drmModePageFlip(drm.fd, drm.crtc_id, fb->fb_id,
+			DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
+	if (ret) {
+		fprintf(stderr, "failed to queue page flip: %s\n", strerror(errno));
+		return -1;
+	}
+
+	while (waiting_for_flip) {
+		FD_ZERO(&fds);
+		FD_SET(drm.fd, &fds);
+
+		ret = select(drm.fd + 1, &fds, NULL, NULL, NULL);
+		if (ret < 0) {
+			if(errno==EINTR) {
+				// EINTR - maybe a terminal switch was executed? Retry...
+				continue;
+			} else {
+				fprintf(stderr, "select err: %s\n", strerror(errno));
+                fflush(stderr);
+				return ret;
+			}
+		} else if (ret == 0) {
+			fprintf(stderr, "select timeout!\n");
+            fflush(stderr);
 			return -1;
 		}
+		
+		drmHandleEvent(drm.fd, &evctx);
+	}
 
-		while (waiting_for_flip) {
-			FD_ZERO(&fds);
-			FD_SET(0, &fds);
-			FD_SET(drm.fd, &fds);
-
-			ret = select(drm.fd + 1, &fds, NULL, NULL, NULL);
-			if (ret < 0) {
-				if(errno==EINTR)
-				{
-					// EINTR - maybe a terminal switch was executed? Retry...
-					continue;
-				}
-				else
-				{
-					printf("select err: %s\n", strerror(errno));
-					return ret;
-				}
-			} else if (ret == 0) {
-				printf("select timeout!\n");
-				return -1;
-			} else if (FD_ISSET(0, &fds)) {
-				fprintf(stderr, "user interrupted!\n");
-				return 0;
-			}
-			drmHandleEvent(drm.fd, &evctx);
-		}
-
-		/* release last buffer to render on again: */
-		gbm_surface_release_buffer(gbm->surface, bo);
-		bo = next_bo;
+	/* release last buffer to render on again: */
+	gbm_surface_release_buffer(gbm->surface, bo);
+	bo = next_bo;
+		
 	return 0;
 }
 
 
 static int legacy_run(const struct gbm *gbm, const struct egl *egl)
 {
-    printf("legacy_run() with legacy_firstframe() and nextframe endless loop");
+    fprintf(stderr, "legacy_run() with legacy_firstframe() and nextframe endless loop");
 	legacy_firstframe(gbm, egl);
 	while (1) {
 		legacy_nextframe(gbm, egl);
