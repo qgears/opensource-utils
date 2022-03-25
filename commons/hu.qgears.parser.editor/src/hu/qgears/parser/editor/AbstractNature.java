60fde958a27806b7338c8a526cf1eef9e289a99d
rizsi <andras.schmidt.hu@gmail.com>
2022-03-25 23:02:20 +0100

    Nature and builder

---
+++ commons/hu.qgears.parser.editor/src/hu/qgears/parser/editor/AbstractNature.java
@@ -0,0 +1,55 @@
+package hu.qgears.parser.editor;
+
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+abstract public class AbstractNature implements IProjectNature {
+	private IProject project;
+	@Override
+	public void configure() throws CoreException {
+		IProjectDescription desc = project.getDescription();
+		ICommand[] commands = desc.getBuildSpec();
+
+		for (int i = 0; i < commands.length; ++i) {
+			if (commands[i].getBuilderName().equals(getBuilderId())) {
+				return;
+			}
+		}
+
+		ICommand[] newCommands = new ICommand[commands.length + 1];
+		System.arraycopy(commands, 0, newCommands, 0, commands.length);
+		ICommand command = desc.newCommand();
+		command.setBuilderName(getBuilderId());
+		newCommands[newCommands.length - 1] = command;
+		desc.setBuildSpec(newCommands);
+		project.setDescription(desc, null);
+	}
+	abstract protected String getBuilderId();
+	@Override
+	public void deconfigure() throws CoreException {
+		IProjectDescription description = getProject().getDescription();
+		ICommand[] commands = description.getBuildSpec();
+		for (int i = 0; i < commands.length; ++i) {
+			if (commands[i].getBuilderName().equals(getBuilderId())) {
+				ICommand[] newCommands = new ICommand[commands.length - 1];
+				System.arraycopy(commands, 0, newCommands, 0, i);
+				System.arraycopy(commands, i + 1, newCommands, i,
+						commands.length - i - 1);
+				description.setBuildSpec(newCommands);
+				project.setDescription(description, null);
+				return;
+			}
+		}
+	}
+	@Override
+	public IProject getProject() {
+		return project;
+	}
+	@Override
+	public void setProject(IProject project) {
+		this.project=project;
+	}
+}
