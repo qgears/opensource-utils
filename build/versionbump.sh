bc6296ceece4fb6e0ee20f348afec4e613cc47c4
Istvan Agoston <agostoni@qgears.com>
2026-08-05 16:46:52 +0200

    Osgi compatible build & finish build migration
     * Update EMF collab's descriptors
     * Synch manifest versions
     * Convert hu.qgears.remote to maven
     * Convert hu.qgears.emfcollab to maven project
     * New versionbump script that automatically synchonizes pom and Manifest versions
     * Remove P2 packageing stuff, which is now unnecessary

---
+++ build/versionbump.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+PARENT_DIR="$(dirname -- "$SCRIPT_DIR")"
+
+#-Dartifacts=rtemplate,hu.rtemplate,rtemplate-parent \
+function error {
+    echo $@
+    exit 1
+}
+
+
+mvn -f ${PARENT_DIR}  \
+-Dartifacts=opensource-utils,opensource-utils-parent \
+org.eclipse.tycho:tycho-versions-plugin:5.0.3:set-version \
+-DnewVersion=$1 \
+-Dversionbump || error "Failed bumping pom versions"
+
+mvn process-classes -Dregenmanifest  || error "Failed update Manifests"
+
