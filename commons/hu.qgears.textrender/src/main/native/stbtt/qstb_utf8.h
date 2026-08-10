95c4dee21335a080b6277d6f069f5df85ddad619
Benedek Sölétormos <benedek.soletormos@qgears.com>
2026-08-10 23:39:22 +0200

    doc comments

--- commons/hu.qgears.textrender/src/main/native/stbtt/qstb_utf8.h
+++ commons/hu.qgears.textrender/src/main/native/stbtt/qstb_utf8.h
@@ -14,8 +14,36 @@ struct utf8 {
     char32_t codepoint;
 };
 
+/**
+ * Gives a usable struct utf8 (with s and len set). Reads the first codepoint.
+ *
+ * @param s will be assigned to s_sunsanitized
+ * @return
+ */
 struct utf8 utf8_init(const uint8_t* s);
+/**
+ * Reads the codepoint at s.end (updating s.off and s.end)
+ *
+ * @param s inited
+ * @return
+ */
 struct utf8 utf8_read(struct utf8 s);
+
+/**
+ * Sets s.off and reads the codepoint at s.off
+ *
+ * @param s inited
+ * @param off
+ * @return
+ */
 struct utf8 utf8_seek(struct utf8 s, size_t off);
+
+/**
+ * Sets s.end to include codepoints based on is_ignore(...)
+ *
+ * @param s inited
+ * @return
+ */
+struct utf8 utf8_include_ignore_chars(struct utf8 s);
 
 #endif
